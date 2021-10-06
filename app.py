4e18b22bbf3f803def97bd7ff8764ac99cd71e26
jhon-swai <jhonswai@gmail.com>
2021-10-07 01:05:24 +0300

    commented out error code

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from app import db
+
+# removed the below code
+# We need to talk more about imports and modules
+# from app import db
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
@@ -10,7 +14,8 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.Datetime, default=datetime.utcnow)
+    # not sure if there is db.Datetime
+    # date_created = db.Column(db.Datetime, default=datetime.utcnow)
     
     def __repr__(self):
         return '<Task %r>' % self.id
